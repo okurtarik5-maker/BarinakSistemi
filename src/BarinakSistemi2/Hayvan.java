a020020052dacadbd70fdfb050357ce04f78cafa
mycurabaz-pixel <mycurabaz@gmail.com>
2026-05-08 16:42:23 +0300

    Hayvan class Update
    Sağlık kaydı eklenmesi

--- src/BarinakSistemi2/Hayvan.java
+++ src/BarinakSistemi2/Hayvan.java
@@ -4,26 +4,16 @@ import java.util.List;
 import java.util.ArrayList;
 
 public abstract class Hayvan {
-	
+
 	private int animalId;
 	private String name;
 	private String tur;
 	private int yas;
 	private String saglikDurumu;
-	private String durum;//yeni eklendi
-	
-	
-	
-	public String getDurum() { // yeni eklendi
-		return durum;
-	}
-
-	public void setDurum(String durum) { // yeni eklendi
-		this.durum = durum;
-	}
+	private String durum;
 
 	private List<SaglikKaydi> saglikKayitlari;
-	
+
 	public Hayvan(int animalId, String name, String tur, int yas, String saglikDurumu) {
 		this.animalId = animalId;
 		this.name = name;
@@ -32,46 +22,60 @@ public abstract class Hayvan {
 		this.saglikDurumu = saglikDurumu;
 		this.saglikKayitlari = new ArrayList<>();
 	}
-	
+
+	public String getDurum() {
+		return durum;
+	}
+	public void setDurum(String durum) {
+		this.durum = durum;
+	}
+
 	public void setAnimalId(int animalId) {
 		this.animalId = animalId;
 	}
 	public int getAnimalId() {
 		return animalId;
 	}
-	
+
 	public void setName(String name) {
 		this.name = name;
 	}
 	public String getName() {
 		return name;
 	}
-	
+
 	public void setTur(String tur) {
 		this.tur = tur;
 	}
 	public String getTur() {
 		return tur;
 	}
-	
+
 	public void setYas(int yas) {
 		this.yas = yas;
 	}
 	public int getYas() {
 		return yas;
 	}
-	
+
 	public void setSaglikDurumu(String saglikDurumu) {
 		this.saglikDurumu = saglikDurumu;
 	}
 	public String getSaglikDurumu() {
 		return saglikDurumu;
 	}
-	
+
 	public abstract String getInfo();
-	
+
 	public void saglikKaydiEkle(SaglikKaydi kayit) {
-		this.saglikKayitlari.add(kayit);		
+		this.saglikKayitlari.add(kayit);
 	}
 
+	public List<SaglikKaydi> getSaglikKayitlari() {
+		return saglikKayitlari;
+	}
+
+	public void setSaglikKayitlari(List<SaglikKaydi> saglikKayitlari) {
+		this.saglikKayitlari = saglikKayitlari;
+	}
 }
